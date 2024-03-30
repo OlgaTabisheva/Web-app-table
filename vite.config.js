0a878ce73c9070d1f70cf1f9f4bdf66fa6413b06
Olga Tabisheva <oliatabisheva@gmail.com>
2024-03-30 21:00:45 +0100

    add github pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react';
 import svgr from '@svgr/rollup';
 
 export default defineConfig({
-  plugins: [react(), svgr()]
+  plugins: [react(), svgr()],
+  base: "/Web-app-table/"
+
 });
